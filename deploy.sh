7453158a0cf0069b96b680fa23ac2926b10af3bd
Shaanan Cohney <shaananc@gmail.com>
2022-02-16 14:00:18 +1100

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+COURSEID=124111
+jekyll b
+git add assets/images/* && git commit -m "updated images" && git push
+rm -r inlines
+mkdir inlines
+OLDIFS=$IFS
+IFS=','
+for paget in staff,staff-information-copy-2; do
+    set -- $paget
+    echo $1.html
+    npm exec juice _site/$1.html inlines/$1.html
+    sed -i '' 's?/assets?https://github.com/shaananc/foa-website/raw/main/assets?g' inlines/$1.html
+    curl -vvv -X PUT -H "Authorization: Bearer $(cat .canvastoken)" https://canvas.lms.unimelb.edu.au/api/v1/courses/$COURSEID/pages/$2 --data-urlencode "wiki_page[body]=\"$(cat inlines/$1.html)\""
+done
+IFS=$OLDIFS
