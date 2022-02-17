f7e8ec17dca4fb5a7a0595b2f189967f7977c9ab
Shaanan Cohney <shaananc@gmail.com>
2022-02-17 21:56:08 +1100

    updates

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,12 @@ rm -r inlines
 mkdir inlines
 OLDIFS=$IFS
 IFS=','
-for paget in staff,staff-information; do
+for paget in staff,staff-information home,welcome; do
     set -- $paget
     echo $1.html
     npm exec juice _site/$1.html inlines/$1.html
     sed -i '' 's?/assets?https://github.com/shaananc/foa-website/raw/main/assets?g' inlines/$1.html
-    curl -vvv -X PUT -H "Authorization: Bearer $(cat .canvastoken)" https://canvas.lms.unimelb.edu.au/api/v1/courses/$COURSEID/pages/$2 --data-urlencode "wiki_page[body]=\"$(cat inlines/$1.html)\""
+    curl -X PUT -H "Authorization: Bearer $(cat .canvastoken)" https://canvas.lms.unimelb.edu.au/api/v1/courses/$COURSEID/pages/$2 --data-urlencode "wiki_page[body]=$(cat inlines/$1.html)"
 done
 IFS=$OLDIFS
+echo '\n'
